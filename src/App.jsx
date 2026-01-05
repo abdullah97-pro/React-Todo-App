33c5ace4e5502842c55339082a1720f8b992521c
abdullah97-pro <abdullahahmadi019@gmail.com>
2026-01-05 08:29:03 +0430

    add edit and toggle completion

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [text, setText] = useState("");
 
+  const [editId, setEditId] = useState(null);
+  const [editText, setEditText] = useState("");
+
   const addTodo = () => {
     if(!text.trim()) return;
 
@@ -19,6 +22,28 @@ function App() {
 
   }
 
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map(todo => todo.id === id ? {...todo, completed:!todo.completed}:todo)
+    );
+  };
+
+  const startEdit = (todo) => {
+    setEditId(todo.id);
+    setEditText(todo.text);
+  };
+
+  const saveEdit = (id) => {
+    setTodos(
+      todos.map(todo => 
+        todo.id === id ? {...todo, text:editText}:todo
+      )
+    );
+    setEditId(null);
+    setEditText("");
+  };
+
+
   return (
     <>
      <input value={text} onChange={e => setText(e.target.value)} placeholder='Enter your text' />
@@ -29,8 +54,21 @@ function App() {
      <ul>
       {
         todos.map(task => (
-          <li key={task.id}>{task.text} 
-          <button onClick={() => setTodos(t => t.id !== task.id)}><i className="fa-solid fa-trash fa-shake" style={{color: "#ff0000"}}></i></button>
+          <li key={task.id}>
+            {editId === task.id ? (
+              <>
+              <input value={editText} onChange={e => setEditText(e.target.value)} />
+              <button onClick={() => saveEdit(task.id)}>Save</button>
+              </>
+            ): (
+              <>
+              <span onClick={() => toggleTodo(task.id)} style={{textDecoration: task.completed ? "line-through" : "none",
+          cursor: "pointer"}}>{task.text}</span>
+                <button onClick={() => startEdit(task)}>Edit</button>
+                <button onClick={() => setTodos(t => t.id !== task.id)}><i className="fa-solid fa-trash fa-shake" style={{color: "#ff0000"}}></i></button>
+              </>
+            )}
+          
           </li>
         ))
       }
